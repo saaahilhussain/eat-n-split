6750d8ebc79b9f896759f4d37fafe4d1a7e86899
saaahilhussain <140058338+saaahilhussain@users.noreply.github.com>
2025-06-10 19:48:10 +0530

    add: form input field - controlled element

--- src/App.js
+++ src/App.js
@@ -44,6 +44,7 @@ export default function App() {
 
 function FriendsList() {
   const friends = initialFriends;
+
   return (
     <ul>
       {friends.map((friend) => (
@@ -84,13 +85,41 @@ function Button({ children, onClick }) {
 }
 
 function FormAddFriend() {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name || !image) return;
+
+    const id = crypto.randomUUID();
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+    console.log(newFriend);
+    setName("");
+    setImage("http://i.pravatar.cc/48");
+  }
+
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleSubmit}>
       <label>Friend name: </label>
-      <input type="text" />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
       <label>Image URL: </label>
-      <input type="text" />
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
 
       <Button>Add</Button>
     </form>
